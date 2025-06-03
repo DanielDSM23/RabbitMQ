7339e71b48fe50a126700c27bf1d8f43d091fc2e
BrianMad7 <134330198+BrianMad7@users.noreply.github.com>
2025-06-03 16:23:45 +0200

    Create producer file

---
+++ src/producer.js
@@ -0,0 +1,39 @@
+// Chargement de la librairie AMQPLIB
+const amqplib = require('amqplib');
+
+const rabbitmq_url = 'amqp://user:password@10.101.6.132:5672'; // Chaîne de connexion à RabbitMQ
+const queue = 'operationsQueue'; // Nom de la queue à utiliser
+
+const message = {
+    "n1": randomNumber(),
+    "n2": randomNumber()
+};
+
+// Fonction pour envoyer un message
+async function send(msg) {
+    // Connexion à RabbitMQ
+    const conn = await amqplib.connect(rabbitmq_url);
+
+    // Créer un channel (connexion logique à RabbitMQ)
+    const channel = await conn.createChannel();
+
+    // Vérifier que la queue existe (la créer sinon)
+    await channel.assertQueue(queue, { 
+        durable: false
+    });
+
+    // Envoyer le message
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)), 
+        { correlationId : Math.random().toString() , replyTo : queue.queue, operation: "add" });
+
+    console.log('[✓] Message envoyé');
+}
+
+
+function randomNumber() {
+    return Math.floor(Math.random() * 9) + 1;
+}
+
+setInterval( () => {
+    send(message); // Appel de la fonction d'envoi    process.exit(0);
+}, 5000)
